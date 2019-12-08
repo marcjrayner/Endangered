37fa9252b1f8daa7057393817441fd2cbf477af2
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-12-08 14:11:30 +0000

    adds Index route, tested in Insomnia

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -4,3 +4,9 @@ db.dropDatabase();
 db.animals.insertOne({
   name: "test"
 })
+db.animals.insertOne({
+  name: "test2"
+})
+db.animals.insertOne({
+  name: "test3"
+})
--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -6,8 +6,17 @@ const createRouter = function (collection) {
   const router = express.Router();
 
   router.get('/', (req, res) => {
-
+    collection
+    .find()
+    .toArray()
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
   });
+
 
   return router;
 };
