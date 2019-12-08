b2580265b882e09daf0016de74c46aafa925768f
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-12-08 13:59:36 +0000

    adds express listener, server.js complete

--- server/server.js
+++ server/server.js
@@ -14,5 +14,9 @@ MongoClient.connect('mongodb://localhost:27017')
     const animalsCollection = db.collection('animals');
     const animalsRouter = createRouter(animalsCollection);
     app.use('/api/animals', animalsRouter)
-  });
+  })
   .catch(console.err);
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
