3ef2348354c9b6610522cc18303ee72d1cff210f
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-12-08 13:58:14 +0000

    adds MongoClient connection to server.js

--- server/server.js
+++ server/server.js
@@ -3,11 +3,16 @@ const app = express();
 const parser = require('body-parser');
 const cors = require('cors');
 app.use(parser.json());
-app.use(cors())
+app.use(cors());
 
-const createRouter = require('./helpers/create_router.js')
+const createRouter = require('./helpers/create_router.js');
 const MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017')
   .then((client) => {
-    const db = client.db('endangered')
+    const db = client.db('endangered');
+    const animalsCollection = db.collection('animals');
+    const animalsRouter = createRouter(animalsCollection);
+    app.use('/api/animals', animalsRouter)
+  });
+  .catch(console.err);
