0f406e04ae1072e830cf4bc89e63ce6d98ae4a5a
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-12-09 13:47:03 +0000

    adds continent.vue

---
+++ client/src/components/Continent.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div id="cont-animal-list-container">
+    <continent v-for="()"
+    <h2></h2>
+    <div id="continent-animal-list">
+      <ul>
+        <animal v-for="(animal, index) in animals.continent" :animal='animal' :key='index'>
+          <button type="button" name="button">{{animal.image}}</button>
+        </animal>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Animal from "./Animal.vue"
+
+export default {
+  name: "continent-animal-list",
+  props: ["animals"],
+  components: {
+    "animal": Animal
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
