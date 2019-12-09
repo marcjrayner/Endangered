c612ea3c9b4c7d2ff881ad2c9bab8f54acbee782
naz_liton <nazrulliton@icloud.com>
2019-12-09 15:58:53 +0000

    get up to date

--- client/src/components/Continent.vue
+++ client/src/components/Continent.vue
@@ -1,19 +1,19 @@
 <template lang="html">
   <div id="cont-animal-list-container">
-<<<<<<< HEAD
+
     <continent v-for="()"
     <h2></h2>
     <div id="continent-animal-list">
       <ul>
         <animal v-for="(animal, index) in animals.continent" :animal='animal' :key='index'>
           <button type="button" name="button">{{animal.image}}</button>
-=======
+
     <h2>Continent Animals</h2>
     <div id="continent-animal-list">
       <ul>
         <animal v-for="(animal, index) in animals.continent" :animal='animal' :key='index'>
           {{animal.image}}
->>>>>>> 33d64f1195276210ba57473a3c077c5ade04c0e7
+
         </animal>
       </ul>
     </div>
