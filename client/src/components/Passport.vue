2919526985a6f156b864e2a2e7e1fa33a9478372
Catrin <catrinhughes92@gmail.com>
2019-12-09 11:44:11 +0000

    answer fact added

--- client/src/components/Passport.vue
+++ client/src/components/Passport.vue
@@ -7,9 +7,10 @@
     </div>
     <div id="left-page">
       <h3>{{animal.quiz_question}}</h3>
-      <ul id="answers" v-for="answer in shuffle(animal.answers)">
-        <li><button type="button">{{answer}}</button></li>
-      </ul>
+        <ul v-for="answer in shuffle(animal.answers)">
+          <li><button @click="checkAnswer(answer)" :class="answer_class" type="button">{{answer}}</button></li>
+        </ul>
+        <p :class="answer_class">{{animal.answer.fact}}</p>
     </div>
   </div>
 </template>
@@ -20,11 +21,12 @@ import Continent from "/Continent.vue";
 export default {
   name: "passport",
   props: ["animal"],
-  // data(){
-  //   return{
-  //     answers: shuffle(this.animal.answers)
-  //   }
-  // },
+  data(){
+    return{
+      // answers: shuffle(this.animal.answers)
+      answer_class: "hidden"
+    }
+  },
   methods: {
     shuffle(array){
       for( i = array.length - 1; i > 0; i--){
@@ -34,10 +36,42 @@ export default {
         array[j] = temp
       }
       return array
+    },
+    checkAnswer(answer){
+      if(answer == this.animal.correct_answer){
+        this.answer_class = correct
+      }
+      else {
+        this.answer_class = incorrect
+      }
     }
   }
 }
 </script>
 
 <style lang="css" scoped>
+
+  p.hidden {
+    visibility: hidden;
+  }
+
+  p.correct {
+    visibility: visible;
+  }
+
+  p.incorrect {
+    visibility: visible;
+  }
+
+  li.hidden {
+    border: 2px solid black
+  }
+
+  li.correct {
+    border: 2px solid green
+  }
+
+  li.incorrect {
+    border: 2px solid red
+  }
 </style>
