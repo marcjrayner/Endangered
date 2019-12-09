b9c682cfeb7f844835961db936cc4be1ee8b865e
naz_liton <nazrulliton@icloud.com>
2019-12-09 14:39:04 +0000

    font awesome added to npm install

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { library } from ‘@fortawesome/fontawesome-svg-core’
+import { faPaw } from ‘@fortawesome/free-solid-svg-icons’
+import { FontAwesomeIcon } from ‘@fortawesome/vue-fontawesome’
+
 Vue.config.productionTip = false
 
 new Vue({
