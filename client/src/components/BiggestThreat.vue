8a2f2a1d2e11e84f66a78305eeaf10f52f482cb6
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-12-11 11:58:12 +0000

    first iteration of displaying biggest threat

--- client/src/components/BiggestThreat.vue
+++ client/src/components/BiggestThreat.vue
@@ -1,6 +1,11 @@
 <template lang="html">
 <div>
   <button @click="moreInfo()"  type="button">More Info</button>
+  <div v-if="biggestThreatObject">
+    {{biggestThreatObject.name}}
+    <hr>
+    {{biggestThreatObject.issue}}
+  </div>
 </div>
 </template>
 
@@ -8,8 +13,8 @@
 import {eventBus} from "../main.js";
 
 export default {
-  name: "biggest-threat"
-  props: ["continents", "threatObjects"],
+  name: "biggest-threat",
+  props: ["biggestThreatObject"],
   methods: {
     moreInfo() {
       eventBus.$emit("more-info")
