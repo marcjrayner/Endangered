90bd2a51b4656b73603b92bb6186cdbc91b26439
naz_liton <nazrulliton@icloud.com>
2019-12-11 10:20:28 +0000

    nav bar placement fixed, now focus on pic

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="app">
+    <div class="sidenav">
+<div class="">
+
+
+<img src="" alt="./assets/white.png">
+<img src="" alt="./assets/white.png">
+<img src="" alt="./assets/white.png">
+</div>
+<button type="button" name="button">Reset</button>
+<button type="button" name="button">Issues</button>
+
+
+</div>
+
+
 
 <world-map :continents="continents"></world-map>
-    
+
   </div>
 </template>
 
@@ -23,18 +38,67 @@ export default {
   mounted(){
     ContinentsService.getContinents()
     .then(continents => this.continents = continents)
-    ;
-  }
+    ;},
+     NavPop() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
 }
+  // ,
+  // methods: {
+  //    openSlideMenu() {
+  //     document.getElementById('menu').style.width='250px';
+  //     document.getElementById('content').style.marginLeft = '250px';
+  //   },
+  //    closeSlideMenu() {
+  //     document.getElementById('menu').style.width='0';
+  //     document.getElementById('content').style.marginLeft = '0';
+  //   }
+  // }
+}
+
+
 </script>
+
 
 <style>
 
 
 body{
 
-background-image: url('./assets/Continents.png');
+background-image: url('./assets/continent.png');
 background-repeat: no-repeat;
 background-size: 2000px 900px
 }
+
+
+
+.sidenav {
+  height: auto%;
+  width: 140px;
+  position: absolute;
+  z-index: 1;
+  top: 500px;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+.sidenav a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+}
+
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+.main {
+  margin-left: 160px;
+  padding: 0px 10px;
+}
+
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faPaw, faCheckCircle, faTimesCircle} from "@fortawesome/free-solid-svg-icons"
+import { faPaw, faCheckCircle, faTimesCircle, faBars} from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
 Vue.config.productionTip = false
